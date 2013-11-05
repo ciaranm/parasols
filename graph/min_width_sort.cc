a753c3e8e96ef1260a71c399cfdd7966b2722968
Ciaran McCreesh <c.mccreesh.1@research.gla.ac.uk>
2013-11-05 22:23:05 +0000

    Implement MWO

--- graph/min_width_sort.cc
+++ graph/min_width_sort.cc
@@ -2,11 +2,36 @@
 
 #include <graph/min_width_sort.hh>
 
+#include <algorithm>
+
 using namespace parasols;
 
-auto parasols::min_width_sort(const Graph &, std::vector<int> &, bool) -> void
+auto parasols::min_width_sort(const Graph & graph, std::vector<int> & p, bool reverse) -> void
 {
-    // todo: implement this...
-    throw "not implemented";
+    // pre-calculate degrees
+    std::vector<int> degrees;
+    std::transform(p.begin(), p.end(), std::back_inserter(degrees),
+            [&] (int v) { return graph.degree(v); });
+
+    std::vector<int> result;
+
+    while (! p.empty()) {
+        auto min = std::min_element(p.begin(), p.end(),
+                [&] (int a, int b) { return degrees[a] < degrees[b] || (degrees[a] == degrees[b] && a > b); });
+
+        result.push_back(*min);
+
+        for (auto & v : p)
+            if (graph.adjacent(*min, v))
+                --degrees[v];
+
+        p.erase(min);
+    }
+
+    if (reverse)
+        std::swap(result, p);
+    else
+        for (auto r = result.rbegin() ; r != result.rend() ; ++r)
+            p.push_back(*r);
 }
 
