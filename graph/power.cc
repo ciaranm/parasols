32d8b365eb8a87cc1999fe45aed243f10cf68374
Ciaran McCreesh <c.mccreesh.1@research.gla.ac.uk>
2013-11-12 10:30:38 +0000

    Better power code

--- graph/power.cc
+++ graph/power.cc
@@ -1,32 +1,52 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 #include <graph/power.hh>
+#include <limits>
 
 using namespace parasols;
 
 Graph parasols::power(const Graph & graph, int n)
 {
-    /* this is spectacularly dumb. we can do a lot better. */
-    Graph result = graph;
+    std::vector<std::vector<int> > distances((graph.size()));
+    for (auto & d : distances)
+        d.resize((graph.size()));
 
-    for (int p = 1 ; p < n ; ++p) {
-        Graph prev_result = result;
+    /* d(m, n) = infinity */
+    for (int i = 0 ; i < graph.size() ; ++i)
+        for (int j = 0 ; j < graph.size() ; ++j)
+            distances[i][j] = std::numeric_limits<int>::max();
 
-        for (int i = 0 ; i < graph.size() ; ++i) {
-            for (int j = 0 ; j < graph.size() ; ++j) {
-                if (i == j)
-                    continue;
+    /* d(m, m) = 0 */
+    for (int i = 0 ; i < graph.size() ; ++i)
+        distances[i][i] = 0;
 
-                if (prev_result.adjacent(i, j)) {
-                    for (int k = 0 ; k < graph.size() ; ++k) {
-                        if (j == k)
-                            continue;
+    /* d(m, n) = 1 if m, n in members and adjacent */
+    for (int i = 0 ; i < graph.size() ; ++i)
+        for (int j = 0 ; j < graph.size() ; ++j)
+            if (i != j && graph.adjacent(i, j))
+                distances[i][j] = 1;
 
-                        if (graph.adjacent(i, k))
-                            result.add_edge(j, k);
-                    }
+    /* shorten */
+    for (int i = 0 ; i < graph.size() ; ++i)
+        for (int j = 0 ; j < graph.size() ; ++j)
+            for (int k = 0 ; k < graph.size() ; ++k) {
+                if (distances[i][k] != std::numeric_limits<int>::max() &&
+                        distances[k][j] != std::numeric_limits<int>::max()) {
+                    int d = distances[i][k] + distances[k][j];
+                    if (distances[i][j] > d)
+                        distances[i][j] = d;
                 }
             }
+
+    Graph result;
+    result.resize(graph.size());
+
+    for (int i = 0 ; i < graph.size() ; ++i) {
+        for (int j = 0 ; j < graph.size() ; ++j) {
+            if (i == j)
+                continue;
+            if (distances[i][j] <= n)
+                result.add_edge(i, j);
         }
     }
 
