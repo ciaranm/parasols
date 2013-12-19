5eddec731fec50b64f08bf6373db0a20dd1b67a5
Ciaran McCreesh <c.mccreesh.1@research.gla.ac.uk>
2013-12-19 10:47:55 +0000

    Support multiple problems in one go

--- programs/solve_max_clique.cc
+++ programs/solve_max_clique.cc
@@ -111,8 +111,9 @@ auto main(int argc, char * argv[]) -> int
 
         po::options_description all_options{ "All options" };
         all_options.add_options()
-            ("algorithm", "Specify which algorithm to use")
-            ("input-file", "Specify the input file (DIMACS format, unless --pairs is specified)")
+            ("algorithm",  "Specify which algorithm to use")
+            ("input-file", po::value<std::vector<std::string> >(),
+                           "Specify the input file (DIMACS format, unless --pairs is specified). May be specified multiple times.")
             ;
 
         all_options.add(display_options);
@@ -120,7 +121,7 @@ auto main(int argc, char * argv[]) -> int
         po::positional_options_description positional_options;
         positional_options
             .add("algorithm", 1)
-            .add("input-file", 1)
+            .add("input-file", -1)
             ;
 
         po::variables_map options_vars;
@@ -139,8 +140,8 @@ auto main(int argc, char * argv[]) -> int
         }
 
         /* No algorithm or no input file specified? Show a message and exit. */
-        if (! options_vars.count("algorithm") || ! options_vars.count("input-file")) {
-            std::cout << "Usage: " << argv[0] << " [options] algorithm file" << std::endl;
+        if (! options_vars.count("algorithm") || options_vars.count("input-file") < 1) {
+            std::cout << "Usage: " << argv[0] << " [options] algorithm file[...]" << std::endl;
             return EXIT_FAILURE;
         }
 
@@ -159,114 +160,122 @@ auto main(int argc, char * argv[]) -> int
             return EXIT_FAILURE;
         }
 
-        /* Figure out what our options should be. */
-        MaxCliqueParams params;
-
-        if (options_vars.count("threads"))
-            params.n_threads = options_vars["threads"].as<int>();
-        else
-            params.n_threads = std::thread::hardware_concurrency();
-
-        if (options_vars.count("stop-after-finding"))
-            params.stop_after_finding = options_vars["stop-after-finding"].as<int>();
-
-        if (options_vars.count("initial-bound"))
-            params.initial_bound = options_vars["initial-bound"].as<int>();
-
-        if (options_vars.count("enumerate"))
-            params.enumerate = true;
-
-        if (options_vars.count("print-incumbents"))
-            params.print_incumbents = true;
-
-        if (options_vars.count("check-club"))
-            params.check_clubs = true;
-
-        if (options_vars.count("split-depth"))
-            params.split_depth = options_vars["split-depth"].as<int>();
-
-        if (options_vars.count("work-donation"))
-            params.work_donation = true;
-
-        if (options_vars.count("donate-when-idle"))
-            params.donate_when_empty = false;
-
-        if (options_vars.count("donation-delay"))
-            params.donation_wait = options_vars["donation-delay"].as<int>();
-
-        if (options_vars.count("min-donation-size"))
-            params.min_donate_size = options_vars["min-donation-size"].as<int>();
-
-        if (options_vars.count("power"))
-            params.power = options_vars["power"].as<int>();
-
-        /* Read in the graph */
-        auto graph = options_vars.count("pairs") ?
-            read_pairs(options_vars["input-file"].as<std::string>()) :
-            read_dimacs(options_vars["input-file"].as<std::string>());
-        params.original_graph = &graph;
-
-        /* Do the actual run. */
-        bool aborted = false;
-        auto result = std::get<1>(*algorithm)(
-                graph,
-                params,
-                aborted,
-                options_vars.count("timeout") ? options_vars["timeout"].as<int>() : 0);
-
-        /* Stop the clock. */
-        auto overall_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - params.start_time);
-
-        /* Display the results. */
-        std::cout << result.size << " " << result.nodes;
-
-        if (options_vars.count("enumerate")) {
-            std::cout << " " << result.result_count;
-            if (options_vars.count("check-club"))
-                std::cout << " " << result.result_club_count;
-        }
-
-        if (aborted)
-            std::cout << " aborted";
-
-        std::cout << std::endl;
-
-        /* Members, and whether it's a club. */
-        for (auto v : result.members)
-            std::cout << graph.vertex_name(v) << " ";
-
-        if (options_vars.count("check-club")) {
-            if (is_club(graph, params.power, result.members))
-                std::cout << "(club)" << std::endl;
+        /* For each input file... */
+        auto input_files = options_vars["input-file"].as<std::vector<std::string> >();
+        bool first = true;
+        for (auto & input_file : input_files) {
+            if (first)
+                first = false;
             else
-                std::cout << "(not club)" << std::endl;
-        }
+                std::cout << "--" << std::endl;
 
-        std::cout << std::endl;
+            /* Figure out what our options should be. */
+            MaxCliqueParams params;
 
-        /* Times */
-        std::cout << overall_time.count();
-        if (! result.times.empty()) {
-            for (auto t : result.times)
-                std::cout << " " << t.count();
-        }
-        std::cout << std::endl;
+            if (options_vars.count("threads"))
+                params.n_threads = options_vars["threads"].as<int>();
+            else
+                params.n_threads = std::thread::hardware_concurrency();
 
-        /* Donation */
-        if (params.work_donation)
-            std::cout << result.donations << std::endl;
+            if (options_vars.count("stop-after-finding"))
+                params.stop_after_finding = options_vars["stop-after-finding"].as<int>();
 
-        if (options_vars.count("verify")) {
-            if (params.power > 1) {
-                if (! is_clique(power(graph, params.power), result.members)) {
-                    std::cerr << "Oops! not a clique" << std::endl;
-                    return EXIT_FAILURE;
-                }
+            if (options_vars.count("initial-bound"))
+                params.initial_bound = options_vars["initial-bound"].as<int>();
+
+            if (options_vars.count("enumerate"))
+                params.enumerate = true;
+
+            if (options_vars.count("print-incumbents"))
+                params.print_incumbents = true;
+
+            if (options_vars.count("check-club"))
+                params.check_clubs = true;
+
+            if (options_vars.count("split-depth"))
+                params.split_depth = options_vars["split-depth"].as<int>();
+
+            if (options_vars.count("work-donation"))
+                params.work_donation = true;
+
+            if (options_vars.count("donate-when-idle"))
+                params.donate_when_empty = false;
+
+            if (options_vars.count("donation-delay"))
+                params.donation_wait = options_vars["donation-delay"].as<int>();
+
+            if (options_vars.count("min-donation-size"))
+                params.min_donate_size = options_vars["min-donation-size"].as<int>();
+
+            if (options_vars.count("power"))
+                params.power = options_vars["power"].as<int>();
+
+            /* Read in the graph */
+            auto graph = options_vars.count("pairs") ?  read_pairs(input_file) : read_dimacs(input_file);
+            params.original_graph = &graph;
+
+            /* Do the actual run. */
+            bool aborted = false;
+            auto result = std::get<1>(*algorithm)(
+                    graph,
+                    params,
+                    aborted,
+                    options_vars.count("timeout") ? options_vars["timeout"].as<int>() : 0);
+
+            /* Stop the clock. */
+            auto overall_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - params.start_time);
+
+            /* Display the results. */
+            std::cout << result.size << " " << result.nodes;
+
+            if (options_vars.count("enumerate")) {
+                std::cout << " " << result.result_count;
+                if (options_vars.count("check-club"))
+                    std::cout << " " << result.result_club_count;
             }
-            else {
-                if (! is_clique(graph, result.members)) {
-                    std::cerr << "Oops! not a clique" << std::endl;
-                    return EXIT_FAILURE;
+
+            if (aborted)
+                std::cout << " aborted";
+
+            std::cout << std::endl;
+
+            /* Members, and whether it's a club. */
+            for (auto v : result.members)
+                std::cout << graph.vertex_name(v) << " ";
+
+            if (options_vars.count("check-club")) {
+                if (is_club(graph, params.power, result.members))
+                    std::cout << "(club)" << std::endl;
+                else
+                    std::cout << "(not club)" << std::endl;
+            }
+
+            std::cout << std::endl;
+
+            /* Times */
+            std::cout << overall_time.count();
+            if (! result.times.empty()) {
+                for (auto t : result.times)
+                    std::cout << " " << t.count();
+            }
+            std::cout << std::endl;
+
+            /* Donation */
+            if (params.work_donation)
+                std::cout << result.donations << std::endl;
+
+            if (options_vars.count("verify")) {
+                if (params.power > 1) {
+                    if (! is_clique(power(graph, params.power), result.members)) {
+                        std::cerr << "Oops! not a clique" << std::endl;
+                        return EXIT_FAILURE;
+                    }
+                }
+                else {
+                    if (! is_clique(graph, result.members)) {
+                        std::cerr << "Oops! not a clique" << std::endl;
+                        return EXIT_FAILURE;
+                    }
                 }
             }
         }
