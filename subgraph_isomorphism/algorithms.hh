20e1320d04527562e9c2a34f49a7e53af11069f4
Ciaran McCreesh <c.mccreesh.1@research.gla.ac.uk>
2015-01-06 13:08:01 +0000

    Remove b and mb

--- subgraph_isomorphism/algorithms.hh
+++ subgraph_isomorphism/algorithms.hh
@@ -4,8 +4,6 @@
 #define PARASOLS_GUARD_SUBGRAPH_ISOMORPHISM_ALGORITHMS_HH 1
 
 #include <subgraph_isomorphism/naive_subgraph_isomorphism.hh>
-#include <subgraph_isomorphism/b_subgraph_isomorphism.hh>
-#include <subgraph_isomorphism/mb_subgraph_isomorphism.hh>
 #include <subgraph_isomorphism/cb_subgraph_isomorphism.hh>
 
 #include <utility>
@@ -14,14 +12,6 @@ namespace parasols
 {
     auto subgraph_isomorphism_algorithms = {
         std::make_pair( std::string{ "naive" },         naive_subgraph_isomorphism ),
-        std::make_pair( std::string{ "blv" },           blv_subgraph_isomorphism ),
-        std::make_pair( std::string{ "blvh" },          blvh_subgraph_isomorphism ),
-        std::make_pair( std::string{ "blvc23" },        blvc23_subgraph_isomorphism ),
-        std::make_pair( std::string{ "blvc23h" },       blvc23h_subgraph_isomorphism ),
-        std::make_pair( std::string{ "blvc23i" },       blvc23i_subgraph_isomorphism ),
-        std::make_pair( std::string{ "badlvc23h" },     badlvc23h_subgraph_isomorphism ),
-
-        std::make_pair( std::string{ "mb" },            mb_subgraph_isomorphism ),
 
         std::make_pair( std::string{ "cb" },            cb_subgraph_isomorphism ),
         std::make_pair( std::string{ "cbj" },           cbj_subgraph_isomorphism )
--- subgraph_isomorphism/subdir.mk
+++ subgraph_isomorphism/subdir.mk
@@ -2,8 +2,6 @@ TARGET := libsubgraph_isomorphism.a
 
 SOURCES := \
 	naive_subgraph_isomorphism.cc \
-	b_subgraph_isomorphism.cc \
-	mb_subgraph_isomorphism.cc \
 	cb_subgraph_isomorphism.cc \
 	subgraph_isomorphism_params.cc \
 	subgraph_isomorphism_result.cc \
