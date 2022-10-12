3c365406758e7ed903c6a804f65a2fa0e0ec316f
Ciaran McCreesh <ciaran.mccreesh@googlemail.com>
2022-10-12 16:37:57 +0100

    Fix for newer compilers

--- max_biclique/clique_cover.hh
+++ max_biclique/clique_cover.hh
@@ -18,7 +18,36 @@ namespace parasols
             const FixedBitGraph<size_> & graph,
             const FixedBitSet<size_> & p,
             std::array<unsigned, size_ * bits_per_word> & p_order,
-            std::array<unsigned, size_ * bits_per_word> & result) -> void;
+            std::array<unsigned, size_ * bits_per_word> & result) -> void
+    {
+        FixedBitSet<size_> p_left = p; // not cliqued yet
+        int clique = 0;                // current clique
+        int i = 0;                     // position in result
+
+        // while we've things left to clique
+        while (! p_left.empty()) {
+            // next clique
+            ++clique;
+            // things that can still be given this clique
+            FixedBitSet<size_> q = p_left;
+
+            // while we can still give something this clique
+            while (! q.empty()) {
+                // first thing we can clique
+                int v = q.first_set_bit();
+                p_left.unset(v);
+                q.unset(v);
+
+                // can't give anything nonadjacent to this the same clique
+                graph.intersect_with_row(v, q);
+
+                // record in result
+                result[i] = clique;
+                p_order[i] = v;
+                ++i;
+            }
+        }
+    }
 }
 
 #endif
--- subgraph_isomorphism/supplemental_graphs.hh
+++ subgraph_isomorphism/supplemental_graphs.hh
@@ -6,6 +6,7 @@
 #include <thread>
 #include <vector>
 #include <atomic>
+#include <cstdlib>
 
 namespace parasols
 {
